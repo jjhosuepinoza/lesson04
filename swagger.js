e30e428dc7d0f63e0dfca51335bb37c8bc0a4731
jjhosuepinoza <97245422+jjhosuepinoza@users.noreply.github.com>
2023-03-17 17:56:03 +0100

    commit

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Temple API'
+    description: 'My First API'
   },
   host: 'lesson04-swagger.onrender.com',
   schemes: ['https']
